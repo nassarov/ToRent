706ed128502454ac45b91d9448444cdcb232d212
Ali Nassar <aliinassar8@gmail.com>
2024-04-13 20:07:20 +0200

    u

--- Apps/Screens/OnBoardScreen.jsx
+++ Apps/Screens/OnBoardScreen.jsx
@@ -8,8 +8,8 @@ export default function OnBoardScreen() {
   return (
     <View>
       <Text>OnBoardScreen</Text>
-      <View style={{ width: hp(5), height: hp(5) }} className="bg-zinc-900 ">
-        <Text>Abdo was here</Text>
+      <View style={{ width: hp(9), height: hp(9) }} className="bg-zinc-900 ">
+        <Text className='text-white'>Abdo was here</Text>
       </View>
     </View>
   );
