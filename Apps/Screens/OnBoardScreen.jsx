f36bf5b16f068cbc289b8abefdce193d02ade827
AbdoMohamamd <abdomhammad618@gmail.com>
2024-04-13 20:00:01 +0300

    Test

--- Apps/Screens/OnBoardScreen.jsx
+++ Apps/Screens/OnBoardScreen.jsx
@@ -1,13 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { heightPercentageToDP as hp , widthPercentageToDP as wp} from 'react-native-responsive-screen'
+import { View, Text } from "react-native";
+import React from "react";
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from "react-native-responsive-screen";
 export default function OnBoardScreen() {
   return (
-    <View >
+    <View>
       <Text>OnBoardScreen</Text>
-        <View style={{width:hp(5) , height:hp(5)}} className='bg-zinc-900 '>
-
-        </View>
+      <View style={{ width: hp(5), height: hp(5) }} className="bg-zinc-900 ">
+        <Text>Abdo was here</Text>
+      </View>
     </View>
-  )
+  );
 }
