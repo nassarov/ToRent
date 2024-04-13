e1867fe193e1907f31c8291b89ccf27afff6cbf9
Ali Nassar <aliinassar8@gmail.com>
2024-04-13 19:55:45 +0200

    up

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import OnBoardScreen from './Apps/Screens/OnBoardScreen';
 export default function App() {
   return (
     <View style={styles.container}>
        <Icon name="heart" size={30} color="red" />
+       <OnBoardScreen/>
       <StatusBar style="auto" />
     </View>
   );
