a4d3d1e8ae6bccee765bc3da54029ae949ddd53e
AbdoMohamamd <abdomhammad618@gmail.com>
2024-04-14 20:08:17 +0300

    Fixing data sending

--- Apps/Screens/Registration/SignUpForToRent.jsx
+++ Apps/Screens/Registration/SignUpForToRent.jsx
@@ -9,22 +9,22 @@ import {
 import React, { useState } from "react";
 import { widthPercentageToDP as wp } from "react-native-responsive-screen";
 import { Formik } from "formik";
-import { Ionicons, Entypo ,EvilIcons} from "@expo/vector-icons";
+import { Ionicons, Entypo, EvilIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 
 export default function SignUpForToRent() {
   const navigation = useNavigation();
-
-  const [loading, setLoading] = useState(false);
   const onSubmitMethod = (values) => {
-    setLoading(true);
-    console.log(values.phone_number);
+    console.log(values)
+    navigation.navigate('signup',{values});
+    
   };
   return (
     <View
       className="items-center "
-      style={{ paddingTop: StatusBar.currentHeight }}
+      
     >
+      <StatusBar />
       <View style={{ width: wp(80) }}>
         <Text className="font-bold text-3xl ">Sign Up For ToRent</Text>
         <Text>
@@ -58,12 +58,15 @@ export default function SignUpForToRent() {
             errors,
           }) => (
             <View>
+              
               <TextInput
                 placeholder="Phone Number"
                 value={values?.phone_number}
                 keyboardType="number-pad"
                 onChangeText={handleChange("phone_number")}
-                className="bg-gray-100 p-3 rounded-lg mt-5"
+                className="bg-gray-300 p-3 rounded-lg mt-5 border-[1px] border-gray-400"
+                
+                
               />
               <TouchableOpacity
                 onPress={handleSubmit}
@@ -82,17 +85,19 @@ export default function SignUpForToRent() {
           onPress={() => {}}
           className="bg-purple-700 rounded-lg p-3 items-center justify-center  flex-row"
         >
-         <EvilIcons name="arrow-right" size={24} color="white" />
+          <EvilIcons name="arrow-right" size={24} color="white" />
           <Text className="text-white font-bold text-base ml-1">
             Continue as a guest
           </Text>
         </TouchableOpacity>
       </View>
-      <TouchableOpacity onPress={() => navigation.push("signup")}>
-        <Text>Next</Text>
-      </TouchableOpacity>
-      
 
+      <View className="flex-row mt-3">
+        <Text>Already Have An Account?</Text>
+        <TouchableOpacity>
+          <Text className="text-purple-700"> Login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
