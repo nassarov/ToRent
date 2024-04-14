23664a21323f519bfacce3253dcc665173e6231d
Qusay17 <kosays174@gmail.com>
2024-04-14 17:30:08 +0300

    Login and Signup

---
+++ Apps/Screens/Registration/SignUpScreens.jsx
@@ -0,0 +1,43 @@
+import { View, Text , StyleSheet, TextInput, TouchableOpacity} from 'react-native'
+import React from 'react'
+
+const SignUpScreens = () => {
+  return (
+    <View >
+        <View style={styles.container}>
+      <Text style={styles.title}>Sign Up</Text>
+      </View>
+      
+      <View>
+        <TextInput placeholder='Name'/>
+
+       < TextInput placeholder='Email' />
+
+       < TextInput placeholder='Password' />
+
+       < TextInput placeholder='Confirm Password' />
+
+       <TouchableOpacity onPress={{}}>Sign Up</TouchableOpacity>
+
+
+      </View>
+    </View>
+  )
+}
+
+export default SignUpScreens
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'flex-start', 
+      alignItems: 'center', 
+      paddingTop: 50, 
+    },
+
+    title: {
+      fontFamily: 'bold',
+      fontSize: 30,
+      textAlign: 'center',
+    },
+  })
