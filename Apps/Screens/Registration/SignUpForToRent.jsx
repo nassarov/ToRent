767d338c5c2666e60ddce1ae39d62dff61ab97d6
Ali Nassar <aliinassar8@gmail.com>
2024-04-15 14:50:14 +0200

    update phone

--- Apps/Screens/Registration/SignUpForToRent.jsx
+++ Apps/Screens/Registration/SignUpForToRent.jsx
@@ -1,58 +1,64 @@
 import React, { useState } from "react";
 import { View, Text, StatusBar, TouchableOpacity } from "react-native";
 import { widthPercentageToDP as wp } from "react-native-responsive-screen";
-import { Ionicons, EvilIcons } from "@expo/vector-icons";
+import { Ionicons, EvilIcons , FontAwesome6} from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import PhoneInput from 'react-native-phone-number-input';
 
 export default function SignUpForToRent() {
+  
+  StatusBar.setBarStyle('light-content', true);
+
   const navigation = useNavigation();
   const [phoneNumber, setPhoneNumber] = useState('');
   const [isValidPhone, setIsValidPhone] = useState(true);
 
   // Function to handle real-time validation of the phone number
   const validatePhoneNumber = (text) => {
-    // Check if any character in the input is not a digit
     const containsNonDigit = /\D/.test(text);
-  
     const allowedPrefixes = ["81", "71", "70", "03", "76"];
     const isValidPrefix = allowedPrefixes.includes(text.slice(0, 2));
   
-    // Check if the input contains only digits and the prefix is valid
     if (!containsNonDigit && text.length <= 8 && isValidPrefix) {
-      setPhoneNumber(text);
-      setIsValidPhone(true);
+      setIsValidPhone(true); 
+      setPhoneNumber(text); 
     } else {
       setIsValidPhone(false);
     }
   };
   
+  
 
   // Function to handle submission of the phone number
-  const onSubmitMethod = () => {
-    if (phoneNumber) {
+const onSubmitMethod = () => {
+  if (isValidPhone) { // Check if the phone number is valid
+    if (phoneNumber.length === 8) { // Check if the phone number consists of exactly 8 digits
       console.log(phoneNumber);
       navigation.navigate("signup", { value: phoneNumber });
     } else {
       setIsValidPhone(false);
     }
-  };
+  } else {
+    setIsValidPhone(false);
+  }
+};
 
   return (
     <View style={{ alignItems: 'center' }}>
       <StatusBar />
-      <View style={{ width: wp(80) }}>
-        <Text style={{ fontWeight: 'bold', fontSize: 24, marginTop: 8, textAlign: 'center' }}>
+      <View style={{ width: wp(80) }} className='items-center justify-center'>
+        <Text className='mt-4' style={{ fontWeight: 'bold', fontSize: 24, marginTop: 8, textAlign: 'center' }}>
           Sign Up For ToRent
         </Text>
-        <Text>
+        <Text className='mt-4'>
           Create a profile, Drive easy, Rent your favorite car and more
         </Text>
-        <Text style={{ fontWeight: 'bold', fontSize: 18 }}>Or</Text>
-        <Text>
+        <Text className='mt-4' style={{ fontWeight: 'bold', fontSize: 18 }}>Or</Text>
+        <Text className='mt-4 mb-4'>
           Create a profile, Show your cars, Grow your business and more
         </Text>
-        <PhoneInput
+        <View className='mb-4'>
+        <PhoneInput 
           defaultCode="LB"
           layout="first"
           value={phoneNumber}
@@ -64,32 +70,31 @@ export default function SignUpForToRent() {
             maxLength: 8,
             keyboardType: 'numeric',
           }}
-          
           onChangeText={validatePhoneNumber}
         />
         {!isValidPhone && (
           <Text style={{ color: 'red', fontSize: 14, marginTop: 4 }}>
             Invalid phone number
           </Text>
-        )}
-        <TouchableOpacity
-          onPress={onSubmitMethod}
-          style={{ backgroundColor: 'purple', borderRadius: 10, padding: 12, alignItems: 'center', justifyContent: 'center', marginTop: 12, flexDirection: 'row' }}
+        )}</View>
+        <TouchableOpacity className='bg-violet-600  ' 
+          onPress={validatePhoneNumber&&onSubmitMethod}
+          style={{ width:wp(70), borderRadius: 10, padding: 12, alignItems: 'center', justifyContent: 'space-around', flexDirection: 'row' }}
         >
-          <Ionicons name="person-outline" size={17} color="white" />
-          <Text style={{ color: 'white', fontWeight: 'bold', marginLeft: 4 }}>
-            Use your phone number
+          <Ionicons name="person-sharp" size={24} color="white" />
+          <Text style={{ color: 'white', fontWeight: 'bold' ,marginLeft: -14}}>
+            Use Your Phone Number
           </Text>
         </TouchableOpacity>
 
         <Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold', marginVertical: 16 }}>Or</Text>
-        <TouchableOpacity
+        <TouchableOpacity className='bg-violet-600 '
           onPress={() => {}}
-          style={{ backgroundColor: 'purple', borderRadius: 10, padding: 12, alignItems: 'center', justifyContent: 'center', flexDirection: 'row' }}
+          style={{ width:wp(70), borderRadius: 10, padding: 12, alignItems: 'center', justifyContent: 'space-around', flexDirection: 'row' }}
         >
-          <EvilIcons name="arrow-right" size={24} color="white" />
-          <Text style={{ color: 'white', fontWeight: 'bold', marginLeft: 4 }}>
-            Continue as a guest
+          <FontAwesome6 name="car-side" size={24} color="white" />
+          <Text style={{ color: 'white', fontWeight: 'bold', marginLeft: -14 }}>
+            Continue As A Guest
           </Text>
         </TouchableOpacity>
       </View>
@@ -97,7 +102,7 @@ export default function SignUpForToRent() {
       <View style={{ flexDirection: 'row', marginTop: 12 }}>
         <Text>Already Have An Account?</Text>
         <TouchableOpacity>
-          <Text style={{ color: 'purple' }}> Login</Text>
+          <Text className='text-violet-600'> Login</Text>
         </TouchableOpacity>
       </View>
     </View>
