d8bf4c22b63424329897ab3dced8316aa8c07268
Ali Nassar <aliinassar8@gmail.com>
2024-04-15 17:44:27 +0200

    phone update

--- Apps/Screens/Registration/SignUpForToRent.jsx
+++ Apps/Screens/Registration/SignUpForToRent.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, StatusBar, TouchableOpacity } from "react-native";
-import { widthPercentageToDP as wp } from "react-native-responsive-screen";
+import { heightPercentageToDP, widthPercentageToDP as wp } from "react-native-responsive-screen";
 import { Ionicons, EvilIcons , FontAwesome6} from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
 import PhoneInput from 'react-native-phone-number-input';
@@ -33,8 +33,9 @@ export default function SignUpForToRent() {
 const onSubmitMethod = () => {
   if (isValidPhone) { // Check if the phone number is valid
     if (phoneNumber.length === 8) { // Check if the phone number consists of exactly 8 digits
-      console.log(phoneNumber);
-      navigation.navigate("signup", { value: phoneNumber });
+      const phoneNumberCCode = `+961${phoneNumber}`;
+      console.log(phoneNumberCCode);
+      navigation.navigate("signup", { value: phoneNumberCCode });
     } else {
       setIsValidPhone(false);
     }
@@ -42,22 +43,26 @@ const onSubmitMethod = () => {
     setIsValidPhone(false);
   }
 };
+StatusBar.setBarStyle('light-content', true);
 
   return (
     <View style={{ alignItems: 'center' }}>
-      <StatusBar />
-      <View style={{ width: wp(80) }} className='items-center justify-center'>
+      <View style={{ width: wp(80) }} className='items-center justify-center mt-14'>
         <Text className='mt-4' style={{ fontWeight: 'bold', fontSize: 24, marginTop: 8, textAlign: 'center' }}>
           Sign Up For ToRent
         </Text>
-        <Text className='mt-4'>
+        {/* <Text className='mt-4'>
           Create a profile, Drive easy, Rent your favorite car and more
         </Text>
         <Text className='mt-4' style={{ fontWeight: 'bold', fontSize: 18 }}>Or</Text>
         <Text className='mt-4 mb-4'>
           Create a profile, Show your cars, Grow your business and more
-        </Text>
-        <View className='mb-4'>
+        </Text> */}
+        <Text className='mt-10 text-center from-neutral-700 text-lg'>
+        Welcome to ToRent! Create a profile to unlock all features and start renting your favorite cars, and more.
+      </Text>
+      
+        <View className='mt-6' style={{height:heightPercentageToDP(9)}}>
         <PhoneInput 
           defaultCode="LB"
           layout="first"
@@ -73,36 +78,43 @@ const onSubmitMethod = () => {
           onChangeText={validatePhoneNumber}
         />
         {!isValidPhone && (
-          <Text style={{ color: 'red', fontSize: 14, marginTop: 4 }}>
+          <Text style={{ color: 'red', fontSize: 14, marginTop: 4 }} >
             Invalid phone number
           </Text>
         )}</View>
+        <View className='my-12'>
         <TouchableOpacity className='bg-violet-600  ' 
           onPress={validatePhoneNumber&&onSubmitMethod}
-          style={{ width:wp(70), borderRadius: 10, padding: 12, alignItems: 'center', justifyContent: 'space-around', flexDirection: 'row' }}
+          style={{ width:wp(70), borderRadius: 10, padding: 12, alignItems: 'center', justifyContent: 'space-evenly', flexDirection: 'row' }}
         >
           <Ionicons name="person-sharp" size={24} color="white" />
-          <Text style={{ color: 'white', fontWeight: 'bold' ,marginLeft: -14}}>
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>
             Use Your Phone Number
           </Text>
         </TouchableOpacity>
 
         <Text style={{ textAlign: 'center', fontSize: 20, fontWeight: 'bold', marginVertical: 16 }}>Or</Text>
+
+        <Text className='mt-4 mb-4 text-center'>
+        Not ready to create an account yet?
+      </Text>
+
         <TouchableOpacity className='bg-violet-600 '
           onPress={() => {}}
-          style={{ width:wp(70), borderRadius: 10, padding: 12, alignItems: 'center', justifyContent: 'space-around', flexDirection: 'row' }}
+          style={{ width:wp(70), borderRadius: 10, padding: 12, alignItems: 'center', justifyContent: 'space-evenly', flexDirection: 'row' }}
         >
           <FontAwesome6 name="car-side" size={24} color="white" />
-          <Text style={{ color: 'white', fontWeight: 'bold', marginLeft: -14 }}>
+          <Text style={{ color: 'white', fontWeight: 'bold'}}>
             Continue As A Guest
           </Text>
         </TouchableOpacity>
+        </View>
       </View>
 
       <View style={{ flexDirection: 'row', marginTop: 12 }}>
         <Text>Already Have An Account?</Text>
-        <TouchableOpacity>
-          <Text className='text-violet-600'> Login</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("login")}>
+          <Text className='text-violet-600 text-[15px] mt-[-2px]'> Login</Text>
         </TouchableOpacity>
       </View>
     </View>
