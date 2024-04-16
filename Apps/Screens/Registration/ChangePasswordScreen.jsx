820d4fd78127330df420daaf917f4180cc3103c0
Qusay17 <kosays174@gmail.com>
2024-04-16 23:47:16 +0300

    ChangePAssword

--- Apps/Screens/Registration/ChangePasswordScreen.jsx
+++ Apps/Screens/Registration/ChangePasswordScreen.jsx
@@ -1,11 +1,185 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  StatusBar,
+} from "react-native";
+import React, { useState } from "react";
+import Icon from "react-native-vector-icons/Ionicons";
+import { Formik } from "formik";
 
 export default function ChangePasswordScreen() {
+  StatusBar.setBarStyle('dark-content', true);
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  // Function to toggle password visibility
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  // Function to toggle confirm password visibility
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
   return (
-    <View>
-      <Text className='font-bold text-2xl'>Change Your Password</Text>
-      <Text></Text>
-    </View>
-  )
+    <Formik
+      initialValues={{
+        password: "",
+        confirmPassword: "",
+      }}
+      onSubmit={(values) => {
+        console.log(values);
+        // Your submission logic goes here
+      }}
+      validate={(values) => {
+        const errors = {};
+        if (!values.password) {
+          errors.password = "Required";
+        }
+        if (!values.confirmPassword) {
+          errors.confirmPassword = "Required";
+        } else if (values.confirmPassword !== values.password) {
+          errors.confirmPassword = "Passwords must match";
+        }
+        return errors;
+      }}
+    >
+      {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
+        <View style={styles.container}>
+          <StatusBar backgroundColor={'#F6F6F6'} translucent={true}/>
+
+
+          <Text style={styles.title}>Change Your Password</Text>
+          <Text style={styles.subtext}>Please type in your new password of your account </Text>
+          {/* Password with Eye Icon */}
+          <View style={{ height: 80 }}>
+            <View style={styles.passwordContainer}>
+              <TextInput
+                placeholder="Password"
+                style={styles.input}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+                secureTextEntry={!showPassword}
+              />
+              <TouchableOpacity
+                style={styles.iconContainer}
+                onPress={togglePasswordVisibility}
+              >
+                <Icon
+                  name={showPassword ? "eye-off" : "eye"}
+                  size={25}
+                  color="#7F5AF0"
+                />
+              </TouchableOpacity>
+            </View>
+            {errors.password && (
+              <Text style={styles.errorText}>{errors.password}</Text>
+            )}
+          </View>
+
+          {/* Confirm Password with Eye Icon */}
+          <View style={{ height: 80 }}>
+            <View style={styles.passwordContainer}>
+              <TextInput
+                placeholder="Retype Password"
+                style={styles.input}
+                onChangeText={handleChange("confirmPassword")}
+                onBlur={handleBlur("confirmPassword")}
+                value={values.confirmPassword}
+                secureTextEntry={!showConfirmPassword}
+              />
+              <TouchableOpacity
+                style={styles.iconContainer}
+                onPress={toggleConfirmPasswordVisibility}
+              >
+                <Icon
+                  name={showConfirmPassword ? "eye-off" : "eye"}
+                  size={25}
+                  color="#7F5AF0"
+                />
+              </TouchableOpacity>
+            </View>
+            {errors.confirmPassword && (
+              <Text style={styles.errorText}>{errors.confirmPassword}</Text>
+            )}
+          </View>
+
+          {/* Sign Up Button */}
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.signupButton}
+              onPress={handleSubmit}
+            >
+              <Text style={styles.signupButtonText}>Confirm</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      )}
+    </Formik>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    paddingTop: 50,
+  },
+  input: {
+    borderColor: "#7F5AF0",
+    borderWidth: 1,
+    borderRadius: 8,
+    backgroundColor: "#F6F6F6",
+    margin: 15,
+    padding: 18,
+  },
+  passwordContainer: {
+    position: "relative",
+  },
+  iconContainer: {
+    position: "absolute",
+    right: 25,
+    top: "50%",
+    transform: [{ translateY: -10 }],
+  },
+  signupButton: {
+    backgroundColor: "#7F5AF0",
+    borderRadius: 8,
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "70%",
+    alignSelf: "center",
+  },
+  signupButtonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  errorText: {
+    color: "red",
+    fontSize: 12,
+    marginLeft: 15,
+  },
+  buttonContainer: {
+    alignItems: "center",
+    marginTop: 20,
+  },
+  title:{
+    fontSize: 24,
+    fontWeight:"bold",
+    margin:15,
+  },
+  subtext:{
+    color:"#7F5AF0",
+margin:15,
+  }
+
+});
