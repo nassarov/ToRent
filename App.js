27942c846bb3c36f3a8bf3edc82b1539f08d1492
Ali Nassar <aliinassar8@gmail.com>
2024-04-17 19:50:22 +0200

    Slider

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import OnBoardScreen from "./Apps/Screens/OnBoardScreen";
 import Registration from "./Apps/Navigations/Registration";
 import 'react-native-gesture-handler';
+import HomeScreen from "./Apps/Screens/HomeScreen/HomeScreen";
 
 const Stack = createStackNavigator();
 
@@ -11,7 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="OnBoard">
-        <Stack.Screen
+        {/* <Stack.Screen
           name="OnBoard"
           component={OnBoardScreen}
           options={{ headerShown: false }}
@@ -19,6 +20,11 @@ export default function App() {
         <Stack.Screen
           name="Registration"
           component={Registration}
+          options={{ headerShown: false }}
+        /> */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
--- Apps/Components/HomeComponents/TopSlider.jsx
+++ Apps/Components/HomeComponents/TopSlider.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import PropTypes from 'deprecated-react-native-prop-types'; // Import PropTypes from the deprecated package
 import { SliderBox } from 'react-native-image-slider-box';
-import TopSlider from '../../Components/HomeComponents/TopSlider';
+
 export default function HomeScreen() {
     const images = [
         
@@ -27,11 +27,20 @@ export default function HomeScreen() {
     );
 
     return (
-        <View className='mt-12'>
-            <View className='flex-row mb-2'>
-            <Text className='text-[#7F5AF0] text-xl font-bold ml-4'>Find</Text><Text className='text-xl'> Your Favorite Car</Text>
+        
+           
+            <View style={{ borderRadius: 12, overflow: 'hidden', marginHorizontal:10}}> 
+            <SliderBox
+                images={images}
+                sliderBoxHeight={180}
+                dotColor="#7F5AF0"
+                inactiveDotColor="gray"
+                paginationBoxVerticalPadding={20}
+                autoplay
+                circleLoop
+
+            />
             </View>
-            <TopSlider/>
-        </View>
+        
     );
 }
