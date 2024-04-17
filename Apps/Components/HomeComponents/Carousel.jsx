5d00a8a84bb2310c2c1d4efa7b0e9f534fbd399b
AbdoMohamamd <abdomhammad618@gmail.com>
2024-04-17 20:51:51 +0300

    Pull

--- Apps/Components/HomeComponents/Carousel.jsx
+++ Apps/Components/HomeComponents/Carousel.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 import { FlatList } from 'react-native-gesture-handler'
 
@@ -8,11 +8,16 @@ export default function Carousel() {
         {id:'2',image:require('./../../../assets/HomeSlider/s1.png')},
         {id:'3',image:require('./../../../assets/HomeSlider/s2.webp')},
         {id:'4',image:require('./../../../assets/HomeSlider/s3.jpg')},
-    ]
+    ];
+    const renderItem=({item,index})=>{
+<View>
+    <Image source={item.image} style={{height:200,width}}/>
+</View>
+    }
   return (
     <View>
       <Text>Carousel</Text>
-      <FlatList data={carouselSlides} renderItem={()=>{<view></view>}}/>
+      <FlatList data={carouselSlides} renderItem={renderItem}/>
     </View>
   )
 }
