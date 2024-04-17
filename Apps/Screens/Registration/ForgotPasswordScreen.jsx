36f8f93b8c6d6e9ce33b04bc59714bdc3f7f5183
AbdoMohamamd <abdomhammad618@gmail.com>
2024-04-17 20:32:32 +0300

    adding otp code input

--- Apps/Screens/Registration/ForgotPasswordScreen.jsx
+++ Apps/Screens/Registration/ForgotPasswordScreen.jsx
@@ -1,108 +1,117 @@
-
-import { View, Text, TextInput, TouchableOpacity,StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 import React from "react";
 
 import { useNavigation } from "@react-navigation/native";
 import Icon from "react-native-vector-icons/Ionicons";
 import Constants from "expo-constants";
-import { heightPercentageToDP, widthPercentageToDP } from "react-native-responsive-screen";
+import {
+  heightPercentageToDP,
+  widthPercentageToDP,
+} from "react-native-responsive-screen";
 import { useState } from "react";
-import PhoneInput from 'react-native-phone-number-input';
+import PhoneInput from "react-native-phone-number-input";
 
 export default function ForgotPasswordScreen() {
   const navigation = useNavigation();
 
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState("");
   const [isValidPhone, setIsValidPhone] = useState(true);
 
   // Function to handle real-time validation of the phone number
   const validatePhoneNumber = (text) => {
     const containsNonDigit = /\D/.test(text);
-    const allowedPrefixes = ["81", "71", "70", "03", "76","78","79"];
+    const allowedPrefixes = ["81", "71", "70", "03", "76", "78", "79"];
     const isValidPrefix = allowedPrefixes.includes(text.slice(0, 2));
-  
+
     if (!containsNonDigit && text.length <= 8 && isValidPrefix) {
-      setIsValidPhone(true); 
-      setPhoneNumber(text); 
+      setIsValidPhone(true);
+      setPhoneNumber(text);
     } else {
       setIsValidPhone(false);
     }
   };
-  
-  
 
   // Function to handle submission of the phone number
-const onSubmitMethod = () => {
-  if (isValidPhone) {
-    if (phoneNumber.length === 8) { // Check if the phone number consists of exactly 8 digits
-      const phoneNumberCCode = `+961${phoneNumber}`;
-      console.log(phoneNumberCCode);
-      navigation.navigate("changepassword", { value: phoneNumberCCode });
+  const onSubmitMethod = () => {
+    if (isValidPhone) {
+      if (phoneNumber.length === 8) {
+        // Check if the phone number consists of exactly 8 digits
+        const phoneNumberCCode = `+961${phoneNumber}`;
+        console.log(phoneNumberCCode);
+        navigation.navigate("recoveraccount", { value: phoneNumberCCode });
+      } else {
+        setIsValidPhone(false);
+      }
     } else {
       setIsValidPhone(false);
     }
-  } else {
-    setIsValidPhone(false);
-  }
-};
-
+  };
 
   return (
     <View style={styles.container}>
-    <View
-      className="flex-1 items-center"
-      style={{ paddingTop: Constants.statusBarHeight }}
-    >
-      <View className="w-full p-2">
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Icon name="chevron-back-outline" size={27} color={"#7F5AF0"} />
-        </TouchableOpacity>
-      </View>
       <View
-        style={{
-          width: widthPercentageToDP(90),
-        }}
+        className="flex-1 items-center"
+        style={{ paddingTop: Constants.statusBarHeight }}
       >
-        <Text className="font-bold text-3xl mb-5">
-          Forgot Password? <Icon name="lock-closed" size={22} color="#7F5AF0" />
-        </Text>
-
-        <Text className='text-[#7F5AF0] mb-8'>
-          Please input your phone number to recover your account
-        </Text>
-        <Text className='text-[18px] font-semibold' >Your Phone Number </Text>
-        <View className='mt-6' style={{height:heightPercentageToDP(9)}}>
-        <PhoneInput 
-          defaultCode="LB"
-          layout="first"
-          value={phoneNumber}
-          countryPickerProps={{
-            countryCodes: ['LB'],
-          }}
-          disableArrowIcon={false} 
-          containerStyle={{ backgroundColor: 'white', borderRadius: 10 }}
-          textInputProps={{ 
-            autoFocus: true,
-            maxLength: 8,
-            keyboardType: 'numeric',
-            selectionColor:'#C5C5C5'
-          }}
-          onChangeText={validatePhoneNumber}
-        />   
-         {!isValidPhone && (
-          <Text style={{ color: 'red', fontSize: 14, marginTop: 4 }} >
-            Invalid phone number
-          </Text>
-        )}
+        <View className="w-full p-2">
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Icon name="chevron-back-outline" size={27} color={"#7F5AF0"} />
+          </TouchableOpacity>
         </View>
-        <View className='items-center mt-10'>
-        <TouchableOpacity onPress={onSubmitMethod} style={styles.recoverButton}>
-          <Text style={styles.recoverButtonText}>Recover Account</Text>
-        </TouchableOpacity>
+        <View
+          style={{
+            width: widthPercentageToDP(90),
+          }}
+        >
+          <Text className="font-bold text-3xl mb-5">
+            Forgot Password?{" "}
+            <Icon name="lock-closed" size={22} color="#7F5AF0" />
+          </Text>
 
+          <Text className="text-[#7F5AF0] mb-8">
+            Please input your phone number to recover your account
+          </Text>
+          <Text className="text-[18px] font-semibold">Your Phone Number </Text>
+          <View className="mt-6" style={{ height: heightPercentageToDP(9) }}>
+            <PhoneInput
+              defaultCode="LB"
+              layout="first"
+              value={phoneNumber}
+              countryPickerProps={{
+                countryCodes: ["LB"],
+              }}
+              disableArrowIcon={false}
+              containerStyle={{ backgroundColor: "white", borderRadius: 10 }}
+              textInputProps={{
+                autoFocus: true,
+                maxLength: 8,
+                keyboardType: "numeric",
+                selectionColor: "#C5C5C5",
+              }}
+              onChangeText={validatePhoneNumber}
+            />
+            {!isValidPhone && (
+              <Text style={{ color: "red", fontSize: 14, marginTop: 4 }}>
+                Invalid phone number
+              </Text>
+            )}
+          </View>
+          <View className="items-center mt-10">
+            <TouchableOpacity
+              onPress={onSubmitMethod}
+              style={styles.recoverButton}
+            >
+              <Text style={styles.recoverButtonText}>Recover Account</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
     </View>
   );
 }
@@ -116,11 +125,10 @@ const styles = StyleSheet.create({
     padding: 18,
     width: heightPercentageToDP(42),
     height: widthPercentageToDP(15),
-    
   },
   container: {
     flex: 1,
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start",
   },
   recoverButton: {
     backgroundColor: "#7F5AF0",
@@ -138,5 +146,4 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
   },
-
 });
--- Apps/Screens/Registration/VerificationCodeScreen.jsx
+++ Apps/Screens/Registration/VerificationCodeScreen.jsx
@@ -1,35 +1,36 @@
-import { View, Text,SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from "react-native";
+import React, { useState } from "react";
+import OtpTextInput from "react-native-text-input-otp";
 
-export default function VerificationCodeScreen (){
+export default function VerificationCodeScreen() {
+  const [otp, setOtp] = useState("");
   return (
-    <SafeAreaView style={{flex: 1 }}>
-<View style={{flex:1 , padding:16}}>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ flex: 1, padding: 16 }}>
+        <Text style={{ marginVertical: 12 }}>Enter Verification code</Text>
+        <Text>We are automatically detecting SMS</Text>
 
-<Text style={{marginVertical: 12}}>Enter Verification code</Text>
-<Text>We are automatically detecting SMS</Text>
-
-<View style={{marginVertical : 22, width:50}}>
-
-
-{/* <OtpInput
-numberOfDigits={4}
-onTextChange={(text) =>console.log(text)}
-focusColor={"red"}
-focusStickBlinkingDuration={400}
-theme={{
-  pinCodeContainerStyle:{
-    width:58,
-    height:58,
-    borderRadius:12,
-  }
-}}
-/> */}
-</View>
-
-  
-</View>
+        <OtpTextInput
+        
+          digits={4}
+          otp={otp}
+          setOtp={(value) => {
+            if (
+              !value.includes(" ") &&
+              !value.includes("-") &&
+              !value.includes(".") &&
+              !value.includes(",")
+            ) {
+              setOtp(value);
+            }
+          }}
+          style={{ height: 50, justifyContent: "center" }}
+          focusedStyle={{
+            backgroundColor: "#e5d0ff",
+            borderColor: "#bf8bff",
+          }}
+        />
+      </View>
     </SafeAreaView>
-  )
+  );
 }
-
