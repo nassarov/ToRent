58ebd4b04d5d91b010aeccdb4e9bc98ce200a298
AbdoMohamamd <abdomhammad618@gmail.com>
2024-05-02 21:03:26 +0300

    signup

--- Apps/Screens/Registration/SignUpScreens.jsx
+++ Apps/Screens/Registration/SignUpScreens.jsx
@@ -13,23 +13,26 @@ import Icon from "react-native-vector-icons/Ionicons";
 import { Formik } from "formik";
 import { Dropdown } from "react-native-element-dropdown";
 import AntDesign from "@expo/vector-icons/AntDesign";
-import { heightPercentageToDP, widthPercentageToDP } from "react-native-responsive-screen";
+import {
+  heightPercentageToDP,
+  widthPercentageToDP,
+} from "react-native-responsive-screen";
 import { useNavigation } from "@react-navigation/native";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import {auth,app} from "../../../firebaseConfig"
+import { auth, app } from "../../../firebaseConfig";
 import { getFirestore, doc, setDoc } from "firebase/firestore"; // Import Firestore functions
 
-export default function SignUpScreen({route}) {
-  StatusBar.setBarStyle('dark-content', true);
- 
+export default function SignUpScreen({ route }) {
+  StatusBar.setBarStyle("dark-content", true);
+
   const navigation = useNavigation();
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const [isFocus, setIsFocus] = useState(false); 
+  const [isFocus, setIsFocus] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [phoneNumber, setPhoneNumber] = useState(route.params?.value);
-  const [userid, setUserid]= useState('');
+  const [userid, setUserid] = useState("");
 
   // Function to toggle password visibility
   const togglePasswordVisibility = () => {
@@ -47,12 +50,16 @@ export default function SignUpScreen({route}) {
     { label: "Rent a car", value: "0" },
   ];
 
-  const handelSignUp = async(values) => {
+  const handelSignUp = async (values) => {
     const auth = getAuth();
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
-  
+
       // Firestore: Add additional user data
       const db = getFirestore(app); // Make sure you have initialized your app
       const userRef = doc(db, "users", user.uid); // Create a document reference with the UID
@@ -60,18 +67,24 @@ export default function SignUpScreen({route}) {
       // Set the user data in Firestore
       await setDoc(userRef, {
         name: values.name,
-        email:email, // this is the authenticated user's email
+        email: email, // this is the authenticated user's email
         phoneNumber: values.phoneNumber,
         role: values.role,
         // any other fields you want to save
       });
-      console.log(userCredential)
+      console.log(userCredential);
+      if (values.role === "1") {
+        navigation.push("address");
+      } else {
+        Alert.alert("Success", "You have successfully signed up!");
+        navigation.push("login");
+      }
       // Navigation or other actions after successful signup
     } catch (error) {
       console.log("Error in signup:", error.message);
       // Handle errors here, such as displaying a notification
     }
-  }
+  };
 
   return (
     <Formik
@@ -81,20 +94,12 @@ export default function SignUpScreen({route}) {
         password: "",
         confirmPassword: "",
         role: "",
-        phoneNumber: phoneNumber ,// Using the phone number from the first screen
-        userid:userid
+        phoneNumber: phoneNumber, // Using the phone number from the first screen
+        userid: userid,
       }}
       onSubmit={(values) => {
         console.log(values);
         handelSignUp(values);
-        if(values.role === "1"){
-          navigation.push('address');
-        }
-        else{
-          Alert.alert("Success", "You have successfully signed up!")
-          navigation.push('login');
-        }
-        
       }}
       validate={(values) => {
         const errors = {};
@@ -103,9 +108,7 @@ export default function SignUpScreen({route}) {
         }
         if (!email) {
           errors.email = "Required";
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)
-        ) {
+        } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
           errors.email = "Invalid email address";
         }
         if (!password) {
@@ -124,93 +127,98 @@ export default function SignUpScreen({route}) {
     >
       {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
         <View style={styles.container}>
-          <StatusBar backgroundColor={'#F6F6F6'} translucent={true}/>
+          <StatusBar backgroundColor={"#F6F6F6"} translucent={true} />
           {/* Input Fields */}
           <View>
             {/* Render Label */}
             {/* Display Phone Number */}
-        <Text className='text-lg text-center'>Phone Number: {values.phoneNumber}</Text>
+            <Text className="text-lg text-center">
+              Phone Number: {values.phoneNumber}
+            </Text>
 
             {/* Name */}
-            <View style={{height:heightPercentageToDP(12)}}>
-            <TextInput
-              placeholder="Name"
-              style={styles.input}
-              onChangeText={handleChange("name")}
-              onBlur={handleBlur("name")}
-              value={values.name}
-            />
-            {errors.name && <Text style={styles.errorText}>{errors.name}</Text>}
+            <View style={{ height: heightPercentageToDP(12) }}>
+              <TextInput
+                placeholder="Name"
+                style={styles.input}
+                onChangeText={handleChange("name")}
+                onBlur={handleBlur("name")}
+                value={values.name}
+              />
+              {errors.name && (
+                <Text style={styles.errorText}>{errors.name}</Text>
+              )}
             </View>
 
             {/* Email */}
-            <View style={{height:heightPercentageToDP(12)}}>
-            <TextInput
-              placeholder="Email"
-              style={styles.input}
-              onChangeText={text =>setEmail(text)}
-              onBlur={handleBlur("email")}
-              value={email}
-            />
-            {errors.email && (
-              <Text style={styles.errorText}>{errors.email}</Text>
-            )}
+            <View style={{ height: heightPercentageToDP(12) }}>
+              <TextInput
+                placeholder="Email"
+                style={styles.input}
+                onChangeText={(text) => setEmail(text)}
+                onBlur={handleBlur("email")}
+                value={email}
+              />
+              {errors.email && (
+                <Text style={styles.errorText}>{errors.email}</Text>
+              )}
             </View>
 
             {/* Password with Eye Icon */}
-            <View style={{height:heightPercentageToDP(12)}}>
-            <View style={styles.passwordContainer}>
-              <TextInput
-                placeholder="Password"
-                style={styles.input}
-                onChangeText={text=>setPassword(text)}
-                onBlur={handleBlur("password")}
-                value={password}
-                secureTextEntry={!showPassword}
-              />
-              <TouchableOpacity
-                style={styles.iconContainer}
-                onPress={togglePasswordVisibility}
-              >
-                <Icon
-                  name={showPassword ? "eye-off" : "eye"}
-                  size={25}
-                  color="#7F5AF0"
+            <View style={{ height: heightPercentageToDP(12) }}>
+              <View style={styles.passwordContainer}>
+                <TextInput
+                  placeholder="Password"
+                  style={styles.input}
+                  onChangeText={(text) => setPassword(text)}
+                  onBlur={handleBlur("password")}
+                  value={password}
+                  secureTextEntry={!showPassword}
                 />
-              </TouchableOpacity>
-            </View>
-            {errors.password && (
-              <Text style={styles.errorText}>{errors.password}</Text>
-            )}
+                <TouchableOpacity
+                  style={styles.iconContainer}
+                  onPress={togglePasswordVisibility}
+                >
+                  <Icon
+                    name={showPassword ? "eye-off" : "eye"}
+                    size={25}
+                    color="#7F5AF0"
+                  />
+                </TouchableOpacity>
+              </View>
+              {errors.password && (
+                <Text style={styles.errorText}>{errors.password}</Text>
+              )}
             </View>
             {/* Confirm Password with Eye Icon */}
-            <View style={{height:heightPercentageToDP(12)}}>
-            <View style={styles.passwordContainer}>
-              <TextInput
-                placeholder="Confirm Password"
-                style={styles.input}
-                onChangeText={handleChange("confirmPassword")}
-                onBlur={handleBlur("confirmPassword")}
-                value={values.confirmPassword}
-                secureTextEntry={!showConfirmPassword}
-              />
-              <TouchableOpacity
-                style={styles.iconContainer}
-                onPress={toggleConfirmPasswordVisibility}
-              >
-                <Icon
-                  name={showConfirmPassword ? "eye-off" : "eye"}
-                  size={25}
-                  color="#7F5AF0"
+            <View style={{ height: heightPercentageToDP(12) }}>
+              <View style={styles.passwordContainer}>
+                <TextInput
+                  placeholder="Confirm Password"
+                  style={styles.input}
+                  onChangeText={handleChange("confirmPassword")}
+                  onBlur={handleBlur("confirmPassword")}
+                  value={values.confirmPassword}
+                  secureTextEntry={!showConfirmPassword}
                 />
-              </TouchableOpacity>
+                <TouchableOpacity
+                  style={styles.iconContainer}
+                  onPress={toggleConfirmPasswordVisibility}
+                >
+                  <Icon
+                    name={showConfirmPassword ? "eye-off" : "eye"}
+                    size={25}
+                    color="#7F5AF0"
+                  />
+                </TouchableOpacity>
+              </View>
+              {errors.confirmPassword && (
+                <Text style={styles.errorText}>{errors.confirmPassword}</Text>
+              )}
             </View>
-            {errors.confirmPassword && (
-              <Text style={styles.errorText}>{errors.confirmPassword}</Text>
-            )}</View>
             <View></View>
             {/* Dropdown Menu */}
-            <View style={{height:heightPercentageToDP(12)}}>
+            <View style={{ height: heightPercentageToDP(12) }}>
               <Dropdown
                 style={[styles.dropdown, isFocus && { borderColor: "#7F5AF0" }]}
                 placeholderStyle={styles.placeholderStyle}
@@ -248,13 +256,13 @@ export default function SignUpScreen({route}) {
             </View>
 
             {/* Sign Up Button */}
-            <View className='items-center text-center justify-center'>
-            <TouchableOpacity
-              style={styles.signupButton}
-              onPress={() => handleSubmit()} 
-            >
-              <Text style={styles.signupButtonText}>Sign Up</Text>
-            </TouchableOpacity>
+            <View className="items-center text-center justify-center">
+              <TouchableOpacity
+                style={styles.signupButton}
+                onPress={() => handleSubmit()}
+              >
+                <Text style={styles.signupButtonText}>Sign Up</Text>
+              </TouchableOpacity>
             </View>
           </View>
         </View>
@@ -277,7 +285,7 @@ const styles = StyleSheet.create({
     margin: 15,
     padding: 18,
     width: heightPercentageToDP(42),
-    height: widthPercentageToDP(15)
+    height: widthPercentageToDP(15),
   },
   passwordContainer: {
     position: "relative",
